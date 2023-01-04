0994615a6f9ba729b935807f50a3d4392bbb1647
Hoaveth <hoaveth.gaming@gmail.com>
2023-01-04 13:21:02 +0800

    Added game stats

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,35 +1,50 @@
 import { Response } from "../components/model/ResponseModel";
-import { api } from "./axiosConfig"
+import { api } from "./axiosConfig";
 
 export const API = {
-    getGameResults: async function (today:string, yesterday:string): Promise<Response | unknown> {
-
-        let response : Response;
-        try{
-            response = await api.request({
-                    url: `https://www.balldontlie.io/api/v1/games?seasons[]=2022&start_date=${yesterday}&end_date=${today}`,
-                    method: 'GET'
-            })
-            console.log(response.data)
-         return response.data;
-        }catch(error){
-            console.log(error)
-            return error;
-        }
-    },
-        getPlayers: async function (): Promise<Response | unknown> {
-
-        let response : Response;
-        try{
-            response = await api.request({
-                    url: `https://www.balldontlie.io/api/v1/players`,
-                    method: 'GET'
-            })
-            console.log(response.data)
-         return response.data;
-        }catch(error){
-            console.log(error)
-            return error;
-        }
+  getGameResults: async function (
+    today: string,
+    yesterday: string
+  ): Promise<Response | unknown> {
+    let response: Response;
+    try {
+      response = await api.request({
+        url: `https://www.balldontlie.io/api/v1/games?seasons[]=2022&start_date=${yesterday}&end_date=${today}`,
+        method: "GET",
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return error;
     }
-}
+  },
+  getPlayers: async function (): Promise<Response | unknown> {
+    let response: Response;
+    try {
+      response = await api.request({
+        url: `https://www.balldontlie.io/api/v1/players`,
+        method: "GET",
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+  getGameStats: async function (): Promise<Response | unknown> {
+    let response: Response;
+    try {
+      response = await api.request({
+        url: `https://www.balldontlie.io/api/v1/stats`,
+        method: "GET",
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  },
+};
